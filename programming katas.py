eff33dc996bd8744c58192ffda6aa37d9020b9e1
sinethembazulu045 <sinethemba.zulu@umuzi.org>
2020-02-05 11:51:02 +0200

    second commit

--- programming katas.py
+++ programming katas.py
@@ -2,3 +2,12 @@ def hello():
     Name = str(input())
     return Name
 print("Hello " + hello())
+
+#Exercise 2 checking if a number is even of odd
+def evenOrOdd():
+    num = int(input("insert a number you want to check"))
+    if(num%2)==0:
+        print("{0}is even".format(num))
+    else:
+        print("{0}is odd" .format(num))
+evenOrOdd()
