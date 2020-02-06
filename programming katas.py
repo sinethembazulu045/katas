91539e8a71cdbbca8daf9e2b6181ff147da69aed
sinethembazulu045 <sinethemba.zulu@umuzi.org>
2020-02-06 10:45:53 +0200

    third commit

--- programming katas.py
+++ programming katas.py
@@ -7,7 +7,16 @@ print("Hello " + hello())
 def evenOrOdd():
     num = int(input("insert a number you want to check"))
     if(num%2)==0:
-        print("{0}is even".format(num))
+        print("{0} is even".format(num))
     else:
-        print("{0}is odd" .format(num))
+        print("{0} is odd" .format(num))
 evenOrOdd()
+
+# Drawing a square
+
+def square():
+    size =int(input("enter a size number"))
+    for i in range(size):
+        print ('#' * size)
+square()
+
