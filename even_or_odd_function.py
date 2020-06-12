0f8d7cfd27af9389f7b773aae3464ac2ad9ab066
sinethembazulu045 <43719195+sinethembazulu045@users.noreply.github.com>
2020-06-12 11:25:24 +0200

    Update even_or_odd_function.py
    updated function

--- even_or_odd_function.py
+++ even_or_odd_function.py
@@ -1,12 +1,9 @@
 
-#even or odd function
+#even or odd function that checks if the number is odd or even number
 def odd_or_even(num): # where num can be even or odd number of my choice.
-    even_output ="{0} is even".format(num)
-    odd_output ="{0} is odd" .format(num)
     if(num%2)==0:
-        return even_output
+        return num, "is even number"
     else:
-        return odd_output
+        return num, "is odd number"
 check =odd_or_even(3)
-
 print(check)
